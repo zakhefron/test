6afe8b8ef7fd6eb9ba56f578f8d697bc50ed9641
zakhefron <zakhefron@gmail.com>
2018-12-05 16:13:00 +0530

    testing

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const uppercase = require('uppercase');
+
+
+describe('',function(){
+	it('',function(){
+		assert.equal(uppercase('hello world'),'HELLO WORLD');
+	});	
+});
+
